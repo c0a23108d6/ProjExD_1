ff5fa560f572b4101ff2ccabea53875c2acb1227
nakajimam <c0a23108d6@edu.teu.ac.jp>
2024-06-11 16:04:39 +0900

    練習問題5

--- pygame_image.py
+++ pygame_image.py
@@ -10,14 +10,17 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    kou3_img = pg.image.load("fig/3.png")
-    kou3_img = pg.transform.flip(kou3_img, True, False)
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        kk_rect = kk_img.get_rect()  #こうかとんrectの抽出
+        kk_rect.center = 300, 200
+        screen.blit(kk_img, kk_rect)  #kk_imgをkk_rectの設定に従って貼り付け
         pg.display.update()
         tmr += 1        
         clock.tick(10)
